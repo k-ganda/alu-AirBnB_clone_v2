0a11a6383a6f3b52242d2a821ea5cd3a73a1adab
k-ganda <k.ganda@alustudent.com>
2023-10-03 15:33:47 +0200

    test_place

--- models/amenity.py
+++ models/amenity.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -19,5 +20,4 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
+    place_amenities = relationship("Place", secondary="place_amenity")
