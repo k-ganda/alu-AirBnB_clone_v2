45de6a4204f92a2a62d20141fa7711c9e433ec69
k-ganda <k.ganda@alustudent.com>
2023-10-19 11:13:51 -0700

    document_module

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,47 +1,49 @@
 #!/usr/bin/python3
 
-"""Script that starts a Flask web application"""
-from flask import Flask
+"""
+Script that starts a Flask web application
+"""
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/', methods=['GET'], strict_slashes=False)
 def hello_hbnb():
-    """Comment"""
+    """Returns a greeting message"""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb', methods=['GET'], strict_slashes=False)
 def hbnb():
-    """Comment"""
+    """Returns the acronym HBNB"""
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<string:text>', methods=['GET'], strict_slashes=False)
 def text_route(text):
-    """Comment"""
+    """Returns the text after the letter 'C' """
     return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', methods=['GET'], strict_slashes=False)
+@app.route('/python/<string:text>', methods=['GET'], strict_slashes=False)
 def text_route_python(text="is cool"):
-    """Comment"""
+    """Returns the text after the word 'Python'"""
     return "Python {}".format(text.replace("_", " "))
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
+@app.route('/number/<int:n>', methods=['GET'], strict_slashes=False)
 def num_route(n):
-    """Comment"""
+    """Returns a message stating that the given input is a number"""
     return "{} is a number".format(n)
 
 
-@app.route('/number_template/<n>', strict_flashes=False)
+@app.route('/number_template/<int:n>', methods=['GET'], strict_slashes=False)
 def num_template(n):
-    """Renders a template displaying given number"""
+    """Renders a template that displays the given number"""
     return render_template("5-number.html", n=n)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
