d20d26e01b4e79fb9cc36dfb4a71f66c90c962d8
k-ganda <k.ganda@alustudent.com>
2023-10-19 10:30:06 -0700

    python_route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,34 +1,54 @@
 #!/usr/bin/python3
 """Script to start a Flask web application."""
 
-
 from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/", strict_slashes=False)
 def hello():
-    return "Hello HBNB!"
+    """Route handler for the root URL ("/").
 
+    Returns:
+        str: The string "Hello HBNB!".
+    """
+    return "Hello HBNB!"
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    """Route handler for the "/hbnb" URL.
 
+    Returns:
+        str: The string "HBNB".
+    """
+    return "HBNB"
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+    """Route handler for the "/c/<text>" URL.
 
+    Args:
+        text (str): The text parameter extracted from the URL.
+
+    Returns:
+        str: The modified string with underscores replaced by spaces and prefixed with "C ".
+    """
+    text = text.replace("_", " ")
+    return f"C {text}"
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
+    """Route handler for the "/python" and "/python/<text>" URLs.
+
+    Args:
+        text (str, optional): The text parameter extracted from the URL. Defaults to "is cool".
+
+    Returns:
+        str: The modified string with underscores replaced by spaces and prefixed with "Python ".
+    """
     text = text.replace("_", " ")
     return f"Python {text}"
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
